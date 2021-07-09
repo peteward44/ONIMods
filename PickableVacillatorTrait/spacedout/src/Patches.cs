5564b397cade2ab5c9a6d5fc7eb427741464064b
pete <peteward44@gmail.com>
2021-07-09 22:20:00 +0100

    PickableVacillatorTrait wip

--- PickableVacillatorTrait/spacedout/src/Patches.cs
+++ PickableVacillatorTrait/spacedout/src/Patches.cs
@@ -1,95 +1,122 @@
 ﻿
 using Harmony;
+using Klei.AI;
+using PeterHan.PLib.UI;
+using STRINGS;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
-
+using TUNING;
+using UnityEngine;
 
 public static class Patches
 {
-	static bool isPlantWild( KPrefabID plant )
+
+	[HarmonyPatch(typeof(GeneShuffler))]
+	[HarmonyPatch("ApplyRandomTrait")]
+	public static class GeneShuffler_ApplyRandomTrait_Patch
 	{
-		if (plant.GetComponent<BasicForagePlantPlanted>() != null)
-		{
-			return true;
-		}
 
-		ReceptacleMonitor receptacleMonitor = plant.GetComponent<ReceptacleMonitor>();
-		if (receptacleMonitor != null)
-		{
-			if (!receptacleMonitor.Replanted)
-			{
-				return true;
-			}
-			
-			var plantablePlot = receptacleMonitor.GetReceptacle();
-			if (plantablePlot != null)
-			{
-				if (plantablePlot.GetComponent<WildPlanterBox>() || plantablePlot.GetComponent<WildFarmTile>())
-				{
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-
-	static int countWildPlants(Room room)
-	{
-		int count = 0;
-		foreach (KPrefabID plant in room.cavity.plants)
-		{
-			if (plant != null && isPlantWild( plant ))
-			{
-				count++;
-			}
-		}
-		return count;
-	}
-
-	static void addBuilding( string category, string ID, string name, string description, string effect)
-	{
-		Strings.Add($"STRINGS.BUILDINGS.PREFABS.{ID.ToUpperInvariant()}.NAME", "<link=\"" + ID + "\">" + name + "</link>");
-		Strings.Add($"STRINGS.BUILDINGS.PREFABS.{ID.ToUpperInvariant()}.DESC", description);
-		Strings.Add($"STRINGS.BUILDINGS.PREFABS.{ID.ToUpperInvariant()}.EFFECT", effect);
-
-		int categoryIndex = TUNING.BUILDINGS.PLANORDER.FindIndex(x => x.category == category);
-		(TUNING.BUILDINGS.PLANORDER[categoryIndex].data as IList<String>)?.Add(ID);
-	}
-
-	[HarmonyPatch(typeof(GeneratedBuildings))]
-	[HarmonyPatch("LoadGeneratedBuildings")]
-	public static class GeneratedBuildings_LoadGeneratedBuildings_Patch
-	{
+		
 		public static void OnLoad()
 		{
-			RoomConstraints.WILDPLANT = new RoomConstraints.Constraint(null, delegate (Room room)
-			{
-				return countWildPlants(room) >= 2;
-			}, 1, STRINGS.ROOMS.CRITERIA.WILDPLANT.NAME, STRINGS.ROOMS.CRITERIA.WILDPLANT.DESCRIPTION);
-
-			RoomConstraints.WILDPLANTS = new RoomConstraints.Constraint(null, delegate (Room room)
-			{
-				return countWildPlants(room) >= 4;
-			}, 1, STRINGS.ROOMS.CRITERIA.WILDPLANTS.NAME, STRINGS.ROOMS.CRITERIA.WILDPLANTS.DESCRIPTION);
 		}
 
-		public static void Prefix()
+		private static void OnDialogClosed(string option)
 		{
-			addBuilding("Food", WildPlanterBoxConfig.ID, WildPlanterBoxConfig.Name, WildPlanterBoxConfig.Description, WildPlanterBoxConfig.Effect);
-			addBuilding("Food", WildFarmTileConfig.ID, WildFarmTileConfig.Name, WildFarmTileConfig.Description, WildFarmTileConfig.Effect);
-		}
-	}
 
-	[HarmonyPatch(typeof(Db), "Initialize")]
-	public static class Database_Techs_Init_Patch
-	{
-		public static void Postfix()
+		}
+
+	
+		public static bool Prefix(GeneShuffler __instance, Worker worker)
 		{
-			Db.Get().Techs.Get("Agriculture").unlockedItemIDs.Add(WildPlanterBoxConfig.ID);
-			Db.Get().Techs.Get("Agriculture").unlockedItemIDs.Add(WildFarmTileConfig.ID);
+			var parent = GameScreenManager.Instance.ssOverlayCanvas.gameObject;
+			var dialog = new PDialog("ModifyItem")
+			{
+				Title = "",
+				DialogClosed = OnDialogClosed,
+				SortKey = 200.0f
+			}.AddButton("ok", "OK", null, PUITuning.Colors.ButtonPinkStyle).
+				AddButton("close", "Cancel", null, PUITuning.Colors.
+				ButtonBlueStyle);
+			//var body = new PGridPanel("ModifyBody")
+			//{
+			//	Margin = new RectOffset(10, 10, 10, 10)
+			//}.AddColumn(new GridColumnSpec()).AddColumn(new GridColumnSpec(0.0f, 1.0f));
+			//body.AddRow(UI.MODIFYDIALOG.CAPTION, new PTextField("Title")
+			//{
+			//	Text = editor.Title,
+			//	MaxLength = 127,
+			//	MinWidth = 512,
+			//	BackColor =
+			//	PUITuning.Colors.DialogDarkBackground,
+			//	TextStyle = PUITuning.Fonts.
+			//	TextLightStyle,
+			//	TextAlignment = TMPro.TextAlignmentOptions.Left
+			//}.AddOnRealize((obj) => titleField = obj0));
+			//body.AddRow(UI.MODIFYDIALOG.DESC, new PTextArea("Description")
+			//{
+			//	LineCount = 8,
+			//	Text = editor.Description,
+			//	MaxLength = 7999,
+			//	MinWidth = 512,
+			//	BackColor = PUITuning.Colors.DialogDarkBackground,
+			//	TextStyle = PUITuning.Fonts.TextLightStyle
+			//}.AddOnRealize((obj) => descriptionField = obj));
+			//body.AddRow(UI.MODIFYDIALOG.IMAGE_PATH, CheckGroup(new PCheckBox("UpdateImage")
+			//{
+			//	CheckSize = new Vector2(16.0f, 16.0f),
+			//	OnChecked = ToggleCheckbox,
+			//	BackColor =
+			//	PUITuning.Colors.DialogDarkBackground,
+			//	CheckColor = PUITuning.Colors.
+			//	ComponentDarkStyle
+			//}.AddOnRealize((obj) => doUpdateImg = obj), new PTextField("PreviewPath")
+			//{
+			//	Text = editor.PreviewPath,
+			//	MaxLength = 512,
+			//	MinWidth = 512,
+			//	BackColor =
+			//	PUITuning.Colors.DialogDarkBackground,
+			//	TextStyle = PUITuning.Fonts.
+			//	TextLightStyle,
+			//	TextAlignment = TMPro.TextAlignmentOptions.Left
+			//}.AddOnRealize((obj) => imagePathField = obj)));
+			//body.AddRow(UI.MODIFYDIALOG.DATA_PATH, CheckGroup(new PCheckBox("UpdateData")
+			//{
+			//	CheckSize = new Vector2(16.0f, 16.0f),
+			//	OnChecked = ToggleCheckbox,
+			//	BackColor =
+			//	PUITuning.Colors.DialogDarkBackground,
+			//	CheckColor = PUITuning.Colors.
+			//	ComponentDarkStyle
+			//}.AddOnRealize((obj) => doUpdateData = obj), new PTextField("DataPath")
+			//{
+			//	Text = editor.DataPath,
+			//	MaxLength = 512,
+			//	MinWidth = 512,
+			//	BackColor =
+			//	PUITuning.Colors.DialogDarkBackground,
+			//	TextStyle = PUITuning.Fonts.
+			//	TextLightStyle,
+			//	TextAlignment = TMPro.TextAlignmentOptions.Left
+			//}.AddOnRealize((obj) => dataPathField = obj)));
+			//body.AddRow(UI.MODIFYDIALOG.PATCHNOTES, new PTextField("PatchNotes")
+			//{
+			//	Text = editor.PatchInfo,
+			//	MaxLength = 512,
+			//	MinWidth = 512,
+			//	BackColor = PUITuning.Colors.DialogDarkBackground,
+			//	TextStyle = PUITuning.Fonts.
+			//	TextLightStyle,
+			//	TextAlignment = TMPro.TextAlignmentOptions.Left
+			//}.AddOnRealize((obj) => patchNotesField = obj));
+			//dialog.Body.AddChild(body);
+			dialog.Show();
+
+
+			return true;
 		}
 	}
 }
--- PickableVacillatorTrait/spacedout/src/PickVacillatorTraitScreen.cs
+++ PickableVacillatorTrait/spacedout/src/PickVacillatorTraitScreen.cs
@@ -1,350 +1,143 @@
 using System;
 using System.Collections.Generic;
+using Klei.AI;
 using STRINGS;
-using TMPro;
+using TUNING;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
-public class PickVacillatorTraitScreen : KScreen, ISim4000ms, ISim1000ms
+public class PickVacillatorTraitScreen : KModalButtonMenu
 {
-	public KButton closeButton;
-	public static PickVacillatorTraitScreen Instance;
-	private bool initialized = false;
+	[SerializeField]
+	private OptionsMenuScreen optionsScreen;
 
+	[SerializeField]
+	private SaveScreen saveScreenPrefab;
+
+	[SerializeField]
+	private LoadScreen loadScreenPrefab;
+
+	[SerializeField]
+	private KButton closeButton;
+
+	[SerializeField]
+	private LocText title;
+
+	[SerializeField]
+	private LocText worldSeed;
+
+	private float originalTimeScale;
+
+	private static PickVacillatorTraitScreen instance;
+
+	public static PickVacillatorTraitScreen Instance => instance;
+
+	public static void DestroyInstance()
+	{
+		instance = null;
+	}
 
 	protected override void OnPrefabInit()
 	{
+		keepMenuOpen = true;
 		base.OnPrefabInit();
-		Instance = this;
+		buttons = new ButtonInfo[1];
+		buttons.Add(new ButtonInfo("TEST", Action.NumActions, OnResume));
+		//buttons = new ButtonInfo[8]
+		//{
+		//	new ButtonInfo(UI.FRONTEND.PAUSE_SCREEN.RESUME, Action.NumActions, OnResume),
+		//	new ButtonInfo(UI.FRONTEND.PAUSE_SCREEN.SAVE, Action.NumActions, OnSave),
+		//	new ButtonInfo(UI.FRONTEND.PAUSE_SCREEN.SAVEAS, Action.NumActions, OnSaveAs),
+		//	new ButtonInfo(UI.FRONTEND.PAUSE_SCREEN.LOAD, Action.NumActions, OnLoad),
+		//	new ButtonInfo(UI.FRONTEND.PAUSE_SCREEN.OPTIONS, Action.NumActions, OnOptions),
+		//	new ButtonInfo(UI.FRONTEND.PAUSE_SCREEN.COLONY_SUMMARY, Action.NumActions, OnColonySummary),
+		//	new ButtonInfo(UI.FRONTEND.PAUSE_SCREEN.QUIT, Action.NumActions, OnQuit),
+		//	new ButtonInfo(UI.FRONTEND.PAUSE_SCREEN.DESKTOPQUIT, Action.NumActions, OnDesktopQuit)
+		//};
+
+		closeButton.onClick += OnResume;
+		instance = this;
+		Show(show: false);
 	}
 
 	protected override void OnSpawn()
 	{
 		base.OnSpawn();
-		base.ConsumeMouseScroll = true;
-		Init();
+	//	((TMP_Text)title).SetText((string)"");
+		try
+		{
+//			string settingsCoordinate = CustomGameSettings.Instance.GetSettingsCoordinate();
+//			string[] array = CustomGameSettings.ParseSettingCoordinate(settingsCoordinate);
+//			((TMP_Text)worldSeed).SetText(string.Format(UI.FRONTEND.PAUSE_SCREEN.WORLD_SEED, settingsCoordinate));
+//			((Component)(object)worldSeed).GetComponent<ToolTip>().toolTip = string.Format(UI.FRONTEND.PAUSE_SCREEN.WORLD_SEED_TOOLTIP, array[1], array[2], array[3]);
+		}
+		catch (Exception arg)
+		{
+//			Debug.LogWarning($"Failed to load Coordinates on ClusterLayout {arg}, please report this error on the forums");
+//			CustomGameSettings.Instance.Print();
+//			Debug.Log("ClusterCache: " + string.Join(",", SettingsCache.clusterLayouts.clusterCache.Keys));
+//			((TMP_Text)worldSeed).SetText(string.Format(UI.FRONTEND.PAUSE_SCREEN.WORLD_SEED, "0"));
+		}
 	}
 
-	public void Init()
+	private void OnResume()
 	{
-		if (!initialized)
-		{
-			initialized = true;
-			Populate();
-			closeButton.onClick += delegate
-			{
-				Show(show: false);
-			};
-			Show(show: false);
-		}
+		Show(show: false);
 	}
 
 	protected override void OnShow(bool show)
 	{
+		//IL_001e: Unknown result type (might be due to invalid IL or missing references)
 		base.OnShow(show);
 		if (show)
 		{
-			ManagementMenu.Instance.CloseAll();
-			AllDiagnosticsScreen.Instance.Show(show: false);
-			RefreshRows();
+			return;
 		}
+		ToolTipScreen.Instance.ClearToolTip(closeButton.GetComponent<ToolTip>());
+	}
+
+	private void OnOptions()
+	{
+		ActivateChildScreen(optionsScreen.gameObject);
+	}
+
+	private void OnSaveAs()
+	{
+		ActivateChildScreen(saveScreenPrefab.gameObject);
+	}
+
+	private void ConfirmDecision(string text, System.Action onConfirm)
+	{
+		base.gameObject.SetActive(value: false);
+		ConfirmDialogScreen confirmDialogScreen = (ConfirmDialogScreen)GameScreenManager.Instance.StartScreen(ScreenPrefabs.Instance.ConfirmDialogScreen.gameObject, base.transform.parent.gameObject);
+		confirmDialogScreen.PopupConfirmDialog(text, onConfirm, OnCancelPopup);
+	}
+
+	private void OnQuit()
+	{
+	//	ConfirmDecision(UI.FRONTEND.MAINMENU.QUITCONFIRM, OnQuitConfirm);
+	}
+
+	private void OnDesktopQuit()
+	{
+	//	ConfirmDecision(UI.FRONTEND.MAINMENU.DESKTOPQUITCONFIRM, OnDesktopQuitConfirm);
+	}
+
+	private void OnCancelPopup()
+	{
+		base.gameObject.SetActive(value: true);
 	}
 
 	public override void OnKeyDown(KButtonEvent e)
 	{
-		if (e.TryConsume(Action.Escape))
+		if (e.TryConsume(Action.Escape) || e.TryConsume(Action.MouseRight))
 		{
-			KMonoBehaviour.PlaySound(GlobalAssets.GetSound("HUD_Click_Close"));
 			Show(show: false);
-			e.Consumed = true;
 		}
-		base.OnKeyDown(e);
-	}
-
-	public override void OnKeyUp(KButtonEvent e)
-	{
-		if (PlayerController.Instance.ConsumeIfNotDragging(e, Action.MouseRight))
+		else
 		{
-			KMonoBehaviour.PlaySound(GlobalAssets.GetSound("HUD_Click_Close"));
-			Show(show: false);
-			e.Consumed = true;
+			base.OnKeyDown(e);
 		}
-		if (!e.Consumed)
-		{
-			base.OnKeyUp(e);
-		}
-	}
-
-	public override float GetSortKey()
-	{
-		return 50f;
-	}
-
-	public void Populate(object data = null)
-	{
-		SpawnRows();
-	}
-
-	private void SpawnRows()
-	{
-		WorldInventory worldInventory = ClusterManager.Instance.GetWorld(ClusterManager.Instance.activeWorldId).worldInventory;
-		allowDisplayCategories.Add(GameTags.MaterialCategories);
-		allowDisplayCategories.Add(GameTags.CalorieCategories);
-		allowDisplayCategories.Add(GameTags.UnitCategories);
-		foreach (Tag materialCategory in GameTags.MaterialCategories)
-		{
-			SpawnCategoryRow(materialCategory, GameUtil.MeasureUnit.mass);
-		}
-		foreach (Tag calorieCategory in GameTags.CalorieCategories)
-		{
-			SpawnCategoryRow(calorieCategory, GameUtil.MeasureUnit.kcal);
-		}
-		foreach (Tag unitCategory in GameTags.UnitCategories)
-		{
-			SpawnCategoryRow(unitCategory, GameUtil.MeasureUnit.quantity);
-		}
-		List<Tag> list = new List<Tag>();
-		foreach (KeyValuePair<Tag, GameObject> categoryRow in categoryRows)
-		{
-			list.Add(categoryRow.Key);
-		}
-		list.Sort((Tag a, Tag b) => a.ProperNameStripLink().CompareTo(b.ProperNameStripLink()));
-		foreach (Tag item in list)
-		{
-			categoryRows[item].transform.SetAsLastSibling();
-		}
-	}
-
-	private void SpawnCategoryRow(Tag categoryTag, GameUtil.MeasureUnit unit)
-	{
-		GameObject gameObject = null;
-		if (!categoryRows.ContainsKey(categoryTag))
-		{
-			GameObject gameObject2 = Util.KInstantiateUI(categoryLinePrefab, rootListContainer, force_active: true);
-			((TMP_Text)gameObject2.GetComponent<HierarchyReferences>().GetReference<LocText>("NameLabel")).SetText(categoryTag.ProperNameStripLink());
-			categoryRows.Add(categoryTag, gameObject2);
-			currentlyDisplayedRows.Add(categoryTag, value: true);
-			units.Add(categoryTag, unit);
-			HierarchyReferences component = gameObject2.GetComponent<HierarchyReferences>();
-			component.GetReference<SparkLayer>("Chart").GetComponent<GraphBase>().axis_x.min_value = 0f;
-			component.GetReference<SparkLayer>("Chart").GetComponent<GraphBase>().axis_x.max_value = 600f;
-			component.GetReference<SparkLayer>("Chart").GetComponent<GraphBase>().axis_x.guide_frequency = 120f;
-			component.GetReference<SparkLayer>("Chart").GetComponent<GraphBase>().RefreshGuides();
-		}
-		gameObject = categoryRows[categoryTag].GetComponent<FoldOutPanel>().container;
-		foreach (Tag item in DiscoveredResources.Instance.GetDiscoveredResourcesFromTag(categoryTag))
-		{
-			if (resourceRows.ContainsKey(item))
-			{
-				continue;
-			}
-			GameObject gameObject3 = Util.KInstantiateUI(resourceLinePrefab, gameObject, force_active: true);
-			HierarchyReferences component2 = gameObject3.GetComponent<HierarchyReferences>();
-			Tuple<Sprite, Color> uISprite = Def.GetUISprite(item);
-			component2.GetReference<Image>("Icon").set_sprite(uISprite.first);
-			((Graphic)component2.GetReference<Image>("Icon")).set_color(uISprite.second);
-			((TMP_Text)component2.GetReference<LocText>("NameLabel")).SetText(item.ProperNameStripLink());
-			Tag targetTag = item;
-			MultiToggle pinToggle = component2.GetReference<MultiToggle>("PinToggle");
-			MultiToggle multiToggle = pinToggle;
-			multiToggle.onClick = (System.Action)Delegate.Combine(multiToggle.onClick, (System.Action)delegate
-			{
-				if (ClusterManager.Instance.activeWorld.worldInventory.pinnedResources.Contains(targetTag))
-				{
-					ClusterManager.Instance.activeWorld.worldInventory.pinnedResources.Remove(targetTag);
-				}
-				else
-				{
-					ClusterManager.Instance.activeWorld.worldInventory.pinnedResources.Add(targetTag);
-					if (DiscoveredResources.Instance.newDiscoveries.ContainsKey(targetTag))
-					{
-						DiscoveredResources.Instance.newDiscoveries.Remove(targetTag);
-					}
-				}
-				RefreshPinnedState(targetTag);
-				pinToggle.ChangeState(ClusterManager.Instance.activeWorld.worldInventory.pinnedResources.Contains(targetTag) ? 1 : 0);
-			});
-			gameObject3.GetComponent<MultiToggle>().onClick = pinToggle.onClick;
-			MultiToggle notifyToggle = component2.GetReference<MultiToggle>("NotificationToggle");
-			MultiToggle multiToggle2 = notifyToggle;
-			multiToggle2.onClick = (System.Action)Delegate.Combine(multiToggle2.onClick, (System.Action)delegate
-			{
-				if (ClusterManager.Instance.activeWorld.worldInventory.notifyResources.Contains(targetTag))
-				{
-					ClusterManager.Instance.activeWorld.worldInventory.notifyResources.Remove(targetTag);
-				}
-				else
-				{
-					ClusterManager.Instance.activeWorld.worldInventory.notifyResources.Add(targetTag);
-				}
-				RefreshPinnedState(targetTag);
-				notifyToggle.ChangeState(ClusterManager.Instance.activeWorld.worldInventory.notifyResources.Contains(targetTag) ? 1 : 0);
-			});
-			component2.GetReference<SparkLayer>("Chart").GetComponent<GraphBase>().axis_x.min_value = 0f;
-			component2.GetReference<SparkLayer>("Chart").GetComponent<GraphBase>().axis_x.max_value = 600f;
-			component2.GetReference<SparkLayer>("Chart").GetComponent<GraphBase>().axis_x.guide_frequency = 120f;
-			component2.GetReference<SparkLayer>("Chart").GetComponent<GraphBase>().RefreshGuides();
-			resourceRows.Add(item, gameObject3);
-			currentlyDisplayedRows.Add(item, value: true);
-			if (units.ContainsKey(item))
-			{
-				Debug.LogError(string.Concat("Trying to add ", item.ToString(), ":UnitType ", units[item], " but units dictionary already has key ", item.ToString(), " with unit type:", unit));
-			}
-			else
-			{
-				units.Add(item, unit);
-			}
-		}
-	}
-
-	private void FilterRowBySearch(Tag tag, string filter)
-	{
-		currentlyDisplayedRows[tag] = PassesSearchFilter(tag, filter);
-	}
-
-	private void SearchFilter(string search)
-	{
-		foreach (KeyValuePair<Tag, GameObject> resourceRow in resourceRows)
-		{
-			FilterRowBySearch(resourceRow.Key, search);
-		}
-		foreach (KeyValuePair<Tag, GameObject> categoryRow in categoryRows)
-		{
-			if (PassesSearchFilter(categoryRow.Key, search))
-			{
-				currentlyDisplayedRows[categoryRow.Key] = true;
-				foreach (Tag item in DiscoveredResources.Instance.GetDiscoveredResourcesFromTag(categoryRow.Key))
-				{
-					if (currentlyDisplayedRows.ContainsKey(item))
-					{
-						currentlyDisplayedRows[item] = true;
-					}
-				}
-			}
-			else
-			{
-				currentlyDisplayedRows[categoryRow.Key] = false;
-			}
-		}
-		EnableCategoriesByActiveChildren();
-		SetRowsActive();
-	}
-
-	private bool PassesSearchFilter(Tag tag, string filter)
-	{
-		filter = filter.ToUpper();
-		string text = tag.ProperName().ToUpper();
-		if (filter != "" && !text.Contains(filter) && !tag.Name.ToUpper().Contains(filter))
-		{
-			return false;
-		}
-		return true;
-	}
-
-	private void EnableCategoriesByActiveChildren()
-	{
-		foreach (KeyValuePair<Tag, GameObject> categoryRow in categoryRows)
-		{
-			if (DiscoveredResources.Instance.GetDiscoveredResourcesFromTag(categoryRow.Key).Count == 0)
-			{
-				currentlyDisplayedRows[categoryRow.Key] = false;
-				continue;
-			}
-			GameObject container = categoryRow.Value.GetComponent<FoldOutPanel>().container;
-			foreach (KeyValuePair<Tag, GameObject> resourceRow in resourceRows)
-			{
-				if (!(resourceRow.Value.transform.parent.gameObject != container))
-				{
-					currentlyDisplayedRows[categoryRow.Key] = currentlyDisplayedRows[categoryRow.Key] || currentlyDisplayedRows[resourceRow.Key];
-				}
-			}
-		}
-	}
-
-	public void RefreshRows()
-	{
-	}
-
-	private void RefreshCharts()
-	{
-		float time = GameClock.Instance.GetTime();
-		float num = 3000f;
-		foreach (KeyValuePair<Tag, GameObject> categoryRow in categoryRows)
-		{
-			HierarchyReferences component = categoryRow.Value.GetComponent<HierarchyReferences>();
-			ResourceTracker resourceStatistic = TrackerTool.Instance.GetResourceStatistic(ClusterManager.Instance.activeWorldId, categoryRow.Key);
-			if (resourceStatistic != null)
-			{
-				SparkLayer reference = component.GetReference<SparkLayer>("Chart");
-				Tuple<float, float>[] array = resourceStatistic.ChartableData(num);
-				if (array.Length != 0)
-				{
-					reference.graph.axis_x.max_value = array[array.Length - 1].first;
-				}
-				else
-				{
-					reference.graph.axis_x.max_value = 0f;
-				}
-				reference.graph.axis_x.min_value = time - num;
-				reference.RefreshLine(array, "resourceAmount");
-			}
-			else
-			{
-				DebugUtil.DevLogError("DevError: No tracker found for resource category " + categoryRow.Key);
-			}
-		}
-		foreach (KeyValuePair<Tag, GameObject> resourceRow in resourceRows)
-		{
-			HierarchyReferences component = resourceRow.Value.GetComponent<HierarchyReferences>();
-			ResourceTracker resourceStatistic2 = TrackerTool.Instance.GetResourceStatistic(ClusterManager.Instance.activeWorldId, resourceRow.Key);
-			if (resourceStatistic2 != null)
-			{
-				SparkLayer reference2 = component.GetReference<SparkLayer>("Chart");
-				Tuple<float, float>[] array2 = resourceStatistic2.ChartableData(num);
-				if (array2.Length != 0)
-				{
-					reference2.graph.axis_x.max_value = array2[array2.Length - 1].first;
-				}
-				else
-				{
-					reference2.graph.axis_x.max_value = 0f;
-				}
-				reference2.graph.axis_x.min_value = time - num;
-				reference2.RefreshLine(array2, "resourceAmount");
-			}
-			else
-			{
-				DebugUtil.DevLogError("DevError: No tracker found for resource " + resourceRow.Key);
-			}
-		}
-	}
-
-	private void SetRowsActive()
-	{
-		foreach (KeyValuePair<Tag, GameObject> categoryRow in categoryRows)
-		{
-			if (categoryRow.Value.activeSelf != currentlyDisplayedRows[categoryRow.Key])
-			{
-				categoryRow.Value.SetActive(currentlyDisplayedRows[categoryRow.Key]);
-			}
-		}
-		foreach (KeyValuePair<Tag, GameObject> resourceRow in resourceRows)
-		{
-			if (resourceRow.Value.activeSelf != currentlyDisplayedRows[resourceRow.Key])
-			{
-				resourceRow.Value.SetActive(currentlyDisplayedRows[resourceRow.Key]);
-			}
-		}
-	}
-
-	public void Sim4000ms(float dt)
-	{
-		RefreshCharts();
-	}
-
-	public void Sim1000ms(float dt)
-	{
-		RefreshRows();
 	}
 }
