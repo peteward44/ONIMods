a768e6edf931a362db21647878861821bbb1c97f
Pete Ward <peteward44@gmail.com>
2022-02-07 22:30:22 +0000

    initial commit for automated grill

---
+++ mods/AutomatedGrill/README.md
@@ -0,0 +1,16 @@
+A new electric grill & gas range that accepts food from a converyor input (and likewise outputs on a conveyor), requiring no dupe interaction. This way you can automate your entire food chain.
+
+[h2]Base game & DLC support[/h2]
+This mod supports both [b]vanilla base game[/b], the [b]"Spaced Out" DLC[/b].
+
+[img=https://raw.githubusercontent.com/peterhaneve/ONIMods/main/Docs/VanillaYes.png][/img][img=https://raw.githubusercontent.com/peterhaneve/ONIMods/main/Docs/Dlc1Yes.png][/img]
+
+[h2]Contribute[/h2]
+Please [b][u]thumbs up & favorite[/u][/b] this so others can find it more easily!
+
+[img=https://i.imgur.com/p7Fv1Z6.gif][/img]
+
+[h2]GitHub[/h2]
+If the mod crashes your game, please open an issue on my GitHub and provide logs files, found in your "My Documents/Klei/Oxygen Not Included" folder.
+
+[url=https://github.com/peteward44/ONIMods/]Github Link[/url]
---
+++ mods/AutomatedGrill/project/src/Patches.cs
@@ -0,0 +1,65 @@
+﻿
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using PeterHan.PLib.Core;
+using PeterHan.PLib.Database;
+using PeterHan.PLib.Options;
+using PeterHan.PLib.PatchManager;
+using PeterHan.PLib.UI;
+
+namespace PW
+{
+	public static class Patches
+	{
+		static void addBuilding(string category, string ID, string name, string description, string effect)
+		{
+			Strings.Add($"STRINGS.BUILDINGS.PREFABS.{ID.ToUpperInvariant()}.NAME", "<link=\"" + ID + "\">" + name + "</link>");
+			Strings.Add($"STRINGS.BUILDINGS.PREFABS.{ID.ToUpperInvariant()}.DESC", description);
+			Strings.Add($"STRINGS.BUILDINGS.PREFABS.{ID.ToUpperInvariant()}.EFFECT", effect);
+
+			int categoryIndex = TUNING.BUILDINGS.PLANORDER.FindIndex(x => x.category == category);
+			(TUNING.BUILDINGS.PLANORDER[categoryIndex].data as IList<String>)?.Add(ID);
+		}
+
+		[HarmonyPatch(typeof(GeneratedBuildings), "LoadGeneratedBuildings")]
+		public static class GeneratedBuildings_LoadGeneratedBuildings_Patch
+		{
+			public static void OnLoad()
+			{
+			}
+
+			public static void Prefix()
+			{
+				addBuilding("Food", AutomatedGrillConfig.ID, AutomatedGrillConfig.Name, AutomatedGrillConfig.Description, AutomatedGrillConfig.Effect);
+			}
+		}
+
+		[HarmonyPatch(typeof(Db), "Initialize")]
+		public static class Database_Techs_Init_Patch
+		{
+			public static void Postfix()
+			{
+				Db.Get().Techs.Get("Agriculture").unlockedItemIDs.Add(AutomatedGrillConfig.ID);
+			}
+		}
+	}
+
+
+	public class MyMod : KMod.UserMod2
+	{
+		public override void OnLoad(Harmony harmony)
+		{
+			base.OnLoad(harmony);
+			PUtil.InitLibrary();
+			LocString.CreateLocStringKeys(typeof(AutomatedGrillConfig));
+			new PLocalization().Register();
+			new POptions().RegisterOptions(this, typeof(AutomatedGrillOptions));
+			new PPatchManager(harmony).RegisterPatchClass(typeof(MyMod));
+			Patches.GeneratedBuildings_LoadGeneratedBuildings_Patch.OnLoad();
+		}
+	}
+}
